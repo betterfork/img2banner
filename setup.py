cf61a63c0bae626675bcaf358a0016513361354e
Better Fork Inc <betterfork@protonmail.com>
2019-06-17 01:12:49 -0400

    hooks

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import sys
 import os
 from distutils.core import setup
@@ -6,12 +7,25 @@ from distutils.core import setup
 from setuptools import setup
 from setuptools.command.develop import develop
 from setuptools.command.install import install
+from utils import Init, w
+from datetime import datetime
+
+
+def write_ts_now(key):
+    with open(key + ".ts", "w") as f:
+        f.write(datetime.now().isoformat())
+
+def _setup():
+    # Init()
+    pass
 
 class PostDevelopCommand(develop):
     """Post-installation for development mode."""
     def run(self):
         # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
-        print("Setting eveything up!")
+        print("post-develop command")
+        write_ts_now("develop")
+        w("Setting eveything up!", "a")
         develop.run(self)
 
 class PostInstallCommand(install):
@@ -19,6 +33,7 @@ class PostInstallCommand(install):
     def run(self):
         # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
         print("post-install command")
+        write_ts_now("install")
         install.run(self)
 
 setup(name='img2banner',
